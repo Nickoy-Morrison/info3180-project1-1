bbe93945bea2d6c081919c80fa74a640031112d0
Javed Wright <jdwright870@gmail.com>
2018-03-11 19:46:50 +0000

    Done project logic

--- app/views.py
+++ app/views.py
@@ -29,18 +29,18 @@ def profile():
                 created = str(datetime.now()).split()[0]
                 
                 photo = newProfileForm.photo.data
-                photo_path = os.path.join(os.getcwd(), app.config["UPLOAD_FOLDER"], secure_filename(photo.filename))
+                photo_name = secure_filename(photo.filename)
                 
-                user = User(firstname, lastname, gender, email, location, bio, created, photo_path)
+                user = User(firstname, lastname, gender, email, location, bio, created, photo_name)
                 
                 db.session.add(user)
                 db.session.commit()
                 
                 write_to(bio, newProfileForm.bio.data)
-                photo.save(photo_path)
+                photo.save(os.path.join(app.config['UPLOAD_FOLDER'],photo_name))
                 
                 flash("Profile Added", "success")
-                return redirect(url_for("home"))
+                return redirect(url_for("profiles"))
             except exc.IntegrityError as e:
                 db.session.rollback()
                 flash("Email address already in use", "danger")
@@ -50,19 +50,44 @@ def profile():
                 flash("Internal Error", "danger")
                 return render_template("create_new_profile.html", newProfileForm = newProfileForm)
         
-        errors = flash_errors(newProfileForm)
+        errors = form_errors(newProfileForm)
         flash(''.join(error+" " for error in errors), "danger")
     return render_template("create_new_profile.html", newProfileForm = newProfileForm)
 
 
+@app.route("/profiles")
+def profiles():
+    users = User.query.all()
+    profiles = []
+    
+    for user in users:
+        profiles.append({"pro_pic": user.photo, "f_name":user.firstname, "l_name": user.lastname, "gender": user.gender, "location":user.location, "id":user.id})
+    
+    return render_template("view_profiles.html", profiles = profiles)
+
+@app.route('/profile/<userid>')
+def inidi_profile(userid):
+    user = User.query.filter_by(id=userid).first()
+    
+    if user is None:
+        return redirect(url_for('home'))
+        
+    bio = read_file(user.bio)
+    return render_template("profile.html", user=user, bio=bio)
 
 def write_to(file_path, data):
     with open(file_path, "w") as stream:
         stream.write(data)
-        
 
+def read_file(filename):
+    data = ""
     
-def flash_errors(form):
+    with open(filename, "r") as stream:
+        data = stream.read()
+        
+    return data
+
+def form_errors(form):
     error_list =[]
     for field, errors in form.errors.items():
         for error in errors:
